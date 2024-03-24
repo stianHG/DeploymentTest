871c21608e38deb226b0c36f618f61f28f27c1f3
stianh-egde <94014798+stianh-egde@users.noreply.github.com>
2024-03-25 00:54:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo!! (Heisann!)
+          Hallo!! (Hola!)
         </p>
         
       </header>
