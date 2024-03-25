5261884276f2600254979f213e42f3c041da3aa0
stianhg <stian@hack-godtfredsen.no>
2024-03-25 01:58:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Velkommen til en ny dag!
+          Velkommen til en ny dag! (igjen)
         </p>
         
       </header>
