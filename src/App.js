b094b36cfe2ea301e6cf7deec79a3ddcc417e2d6
stianh-egde <94014798+stianh-egde@users.noreply.github.com>
2024-04-18 08:53:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Velkommen til en ny dag! (2024-03-25 22:10)
+          Velkommen til en ny dag! (2024-04-18 08:53)
         </p>
         
       </header>
